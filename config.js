a00d4f6b54eca35abf30ee35aa143a684ea95a0e
ThaiA12 <92841249+ThaiA12@users.noreply.github.com>
2021-10-20 13:41:07 +0700

    Update config.js

--- config.js
+++ config.js
@@ -1,19 +1,17 @@
 const CONFIG = {
     introTitle: 'Chào các bạn nữ 12A12 nha',
     introDesc: `Hôm nay là ngày 20-10 nên con trai 
-    lớp 12A12 có món quà dành tặng cho các cậu
-    Lạc vào tim anh lẻ loi
-    Đằng sau chữ yêu đây là thương`,
+    lớp 12A12 có món quà dành tặng cho các cậu`,
     btnIntro: 'hihi',
     title: 'happy women day ',
     desc: 'Các cậu có muốn nhận quà không ? ',
     btnYes: 'Không :3',
     btnNo: 'Có :))',
-    question:'Trên thế giới hơn 7 tỉ người nhưng mà nụ cười của các bạn nữ A12 là xinh nhất',
+    question:'Bọn mình đùa thôi 🤣',
     btnReply: 'Tặng các bạn',
     reply: 'Bọn tớ chỉ muốn nói là',
     mess: 'Các cậu luôn là những đóa hoa trong mắt bọn tớ Và Chúc các bạn luôn xinh đẹp , đạt được mơ ước đỗ vào trường mà mình mơ ước',
     messDesc: 'Mong rằng bọn mình sẽ sớm gặp nhau để làm nồi lẩu nhó',
     btnAccept: 'Okiiiii nha,haha',
-    messLink: 'https://www.facebook.com/profile.php?id=100016275974205' 
+    messLink: 'https://www.facebook.com/profile.php?id=100028462939686' 
 }
