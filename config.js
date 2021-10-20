39a10125311f835e1ea91d8ad42ddaae33716aa2
lthaiA12 <92859539+lthaiA12@users.noreply.github.com>
2021-10-20 22:24:06 +0700

    Update config.js

--- config.js
+++ config.js
@@ -8,10 +8,11 @@ const CONFIG = {
     btnYes: 'Không :3',
     btnNo: 'Có :))',
     question:'Ha ha Bị lừa rồi',
-    btnReply: 'Bấm vào đây sẽ là món quà của tui mình',
+    btnReply: 'Bấm vào đây sẽ là món quà của tụi mình',
     reply: 'Bọn tớ chỉ muốn nói là',
-    mess: 'Các cậu luôn là những đóa hoa trong mắt bọn tớ Và Chúc các bạn luôn xinh đẹp , đạt được mơ ước đỗ vào trường mà mình mơ ước',
-    messDesc: 'Mong rằng bọn mình sẽ sớm gặp nhau để làm nồi lẩu nhó',
+    mess: 'Sự có mặt của các bạn nữ trên thế giới này đúng là 1 món quà mà ông trời đã trao tặng.Hôm nay là ngày 20/10 tôi chúc các bạn có một ngày thật hạnh phúc bên người yêu thương,luôn vui cười,luôn xinh đẹp rạng rỡ thành công trên con đường mà các bạn đã chọn và đặc biệt luôn có sức khoẻ để viết tiếp những giấc mơ của mình nha
+',
+    messDesc: 'Mong rằng bọn mình sẽ sớm gặp nhau để làm 1 bữa liên hoành tráng nhó',
     btnAccept: 'Okiiiii nha haha',
     messLink: 'https://www.facebook.com/nguyennangvu2004' 
 }
