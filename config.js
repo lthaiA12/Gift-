75cbbb1f0c25f7d20f3b79708c5f4b6fb0509441
lthaiA12 <92859539+lthaiA12@users.noreply.github.com>
2021-10-20 23:07:24 +0700

    Update config.js

--- config.js
+++ config.js
@@ -13,5 +13,5 @@ const CONFIG = {
     mess: 'Chúc các bạn Nữ  luôn xinh đẹp và đỗ vào trường Đại học mà mình mong ước ',
     messDesc: 'Mong rằng bọn mình sẽ sớm gặp nhau để làm 1 bữa liên hoành tráng nhó',
     btnAccept: 'Okiiiii nha haha',
-    messLink: 'https://www.facebook.com/nguyennangvu2004' 
+    messLink: 'https://www.tiktok.com/@boy12a12/video/7021180066226834714?_d=secCgwIARCbDRjEFSADKAESPgo8OBApn4WqaOaiSEoe7a3Uqdi2OB4VLnmGDBqdkXz4pECtQbC9IQS4sGvO5Z5G0M2psdC5Z4J6treYWN9hGgA%3D&language=vi&mid=0&preview_pb=0&region=VN&sec_user_id=MS4wLjABAAAAwnAbJoMvK5FCB7fv-CjSBDd5MhZU1YbSp4AZjVZVRduYPcVbkMbk2ZwcbRjGYmw2&share_app_id=1180&share_item_id=7021180066226834714&share_link_id=1e6ad9b9-d934-4db6-9c1c-7f4b3c889bed&source=h5_t&timestamp=1634745892&u_code=dl8ikjmca68j1l&user_id=7021085561222349826&utm_campaign=client_share&utm_medium=android&utm_source=copy&_r=1' 
 }
