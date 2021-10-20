db167cb9fbf78f0d75c395c36325a38aad04b74c
lthaiA12 <92859539+lthaiA12@users.noreply.github.com>
2021-10-20 22:48:01 +0700

    Update config.js

--- config.js
+++ config.js
@@ -10,7 +10,7 @@ const CONFIG = {
     question:'Ha ha Bị lừa rồi',
     btnReply: 'Bấm vào đây sẽ là món quà của tụi mình',
     reply: 'Bọn tớ chỉ muốn nói là',
-    mess: 'Nhân ngày 20/10 ngày thật đặc biệt tôi chúc tất cả các bạn nữ luôn luôn hạnh phúc xinh đẹp và luôn có 1 sức khoẻ dồi dào nha ',
+    mess: 'Chúc các bạn Nữ  luôn xinh đẹp và đỗ vào trường Đại học mà mình mong ước ',
     messDesc: 'Mong rằng bọn mình sẽ sớm gặp nhau để làm 1 bữa liên hoành tráng nhó',
     btnAccept: 'Okiiiii nha haha',
     messLink: 'https://www.facebook.com/nguyennangvu2004' 
